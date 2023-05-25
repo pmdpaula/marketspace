6462b84ff31e3e8cf210689fe48b4b0be587eb65
Pedro de Paula <pmdpaula@gmail.com>
2023-05-25 15:13:35 -0300

    feat(service): interceptador de erro no serviço de api

--- src/services/api.ts
+++ src/services/api.ts
@@ -113,4 +113,15 @@ api.registerInterceptTokenManager = (signOut: SignOut) => {
   };
 };
 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.data) {
+      return Promise.reject(new AppError(error.response.data.message));
+    } else {
+      return Promise.reject(error);
+    }
+  },
+);
+
 export default api;
